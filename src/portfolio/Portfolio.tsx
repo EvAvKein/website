b1fcccae8a39fa91c2bae2b5da614883bcacc403
Eve Keinan <evavkein@gmail.com>
2025-10-17 19:02:13 +0300

    Portfolio page additions and improvements

--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -1,52 +1,44 @@
 import "./Portfolio.css";
 import ProjectDisplay from "./ProjectDisplay";
+import {DisplayProject, displayProjects, ListProject, listProjects} from "./projects";
+import ProjItem from "./ProjItem";
 
 function Portfolio() {
 	return (
 		<section id="portfolio">
-				<h2>Projects</h2>
-				<section id="projects">
-					<ProjectDisplay
-						title="MiniRayTracer"
-						tagline="Raytracing your scenes from scratch: Texture mapping, multi-threaded incremental rendering, live movement and object manipulation, and more!"
-						imgSrc="https://raw.githubusercontent.com/EvAvKein/hive_minirt/refs/heads/main/README_assets/eve_wallpaper.png"
-						imgAlt="Colorful scene with patterned spheres, cylinders, planes, a cone, and a light source in the center shedding light on all of them"
-						vidSrc="/src/assets/projects/miniRT_compilation.mp4"
-						techsList={new Set(["C"])}
-						selectedTechs={new Set()}
-						link="https://github.com/EvAvKein/hive_minirt"
-					/>
-					<ProjectDisplay
-						title="Planet Guardian"
-						tagline="A game-jam award winner: Protect your planet from asteroids and temperatures!"
-						imgSrc="/src/assets/projects/planetGuardian.png"
-						imgAlt="Colorful scene with patterned spheres, cylinders, planes, a cone, and a light source in the center shedding light on all of them"
-						vidSrc="/src/assets/projects/planetGuardian_compilation.mp4"
-						techsList={new Set(["C"])}
-						selectedTechs={new Set()}
-						link="https://github.com/EvAvKein/planet_guardian"
-					/>
-					<ProjectDisplay
-						title="So Long"
-						tagline="A 2D game made with a minimalist windowing library: Collect the berries and reach the exit! Steppable flood-fill map validation, defeatable chasing enemies"
-						imgSrc="/src/assets/projects/soLong.png"
-						imgAlt="Colorful scene with patterned spheres, cylinders, planes, a cone, and a light source in the center shedding light on all of them"
-						vidSrc="/src/assets/projects/soLong_compilation.mp4"
-						techsList={new Set(["C"])}
-						selectedTechs={new Set()}
-						link="https://github.com/EvAvKein/hive_so_long"
-					/>
-					<ProjectDisplay
-						title="Philosophers"
-						tagline="Simulating the Dining Philosophers problem: Manual multi-threading synchronization with mutexes"
-						imgSrc="/src/assets/projects/philosophers.png"
-						imgAlt="Colorful scene with patterned spheres, cylinders, planes, a cone, and a light source in the center shedding light on all of them"
-						vidSrc="/src/assets/projects/philosophers_compilation.mp4"
-						techsList={new Set(["C"])}
-						selectedTechs={new Set()}
-						link="https://github.com/EvAvKein/hive_philosophers"
-					/>
-				</section>
+			<h2>Projects</h2>
+			<section id="projDisplays">
+				{displayProjects.map((proj: DisplayProject) => {
+					return (
+						<ProjectDisplay
+							key={proj.name}
+							title={proj.name}
+							tagline={proj.description}
+							imgSrc={proj.logoSrc}
+							imgAlt={proj.logoAlt}
+							vidSrc={proj.vidSrc}
+							techsList={proj.techsSet}
+							link={proj.link}
+						/>
+					);
+				})}
+			</section>
+			<section id="moreProjects">
+				{listProjects.map((proj: ListProject) => {
+					return (
+						<ProjItem
+							key={proj.name}
+							logoSrc={proj.logoSrc}
+							logoAlt={proj.logoAlt}
+							name={proj.name}
+							description={proj.description}
+							technologies={proj.technologies}
+							link={proj.link}
+							selectedTechs={new Set()}
+						/>
+					);
+				})}
+			</section>
 		</section>
 	);
 }
