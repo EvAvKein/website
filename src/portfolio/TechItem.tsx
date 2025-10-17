8373a222b397e3483847a521d3d8ea0c7ef57888
Eve Keinan <evavkein@gmail.com>
2025-10-17 19:01:19 +0300

    TechItem component simplifying

--- src/portfolio/TechItem.tsx
+++ src/portfolio/TechItem.tsx
@@ -1,16 +1,16 @@
-import {type tech, type techsSet} from "./techs";
-import TechSelectionButton from "./TechSelectionButton";
+import {type tech} from "./techs";
+// import TechSelectionButton from "./TechSelectionButton";
 import "./TechItem.css";
 
-function TechItem(props: {techName: tech; techSrc: string; selectedTechs: techsSet}) {
+function TechItem(props: {techName: tech; techSrc: string}) {
 	return (
-		<div className={"techItem" + (props.selectedTechs.has(props.techName) ? " selected" : "")}>
-			<TechSelectionButton techs={new Set([props.techName])} selectedTechs={props.selectedTechs}>
-				<>
-					<img src={props.techSrc} alt={props.techName + "'s logo"} />
-					<div>{props.techName}</div>
-				</>
-			</TechSelectionButton>
+		<div className={"techItem" /*+ (props.selectedTechs.has(props.techName) ? " selected" : "")*/}>
+			{/* <TechSelectionButton techs={new Set([props.techName])} selectedTechs={props.selectedTechs}>
+				<> */}
+			<img src={props.techSrc} alt={props.techName + "'s logo"} />
+			<div>{props.techName}</div>
+			{/* </>
+			</TechSelectionButton> */}
 		</div>
 	);
 }
