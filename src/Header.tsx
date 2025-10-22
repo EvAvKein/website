90a9cc1abd9a7aa180b43bea82d608c68a4cb38c
Eve Keinan <evavkein@gmail.com>
2025-10-22 11:52:24 +0300

    Add skip-to-content nav button

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,20 +3,25 @@ import "./Header.css";
 
 function Header() {
 	return (
-		<header>
-			<a href="#">
-				<img
-					src={eakLogo}
-					alt="The initials E.A.K, with E and K rotated and aligned to neatly overlap with the A inbetween"
-				/>
+		<>
+			<a id="skipToContent" href="#main">
+				<div>Skip to content</div>
 			</a>
-			<a href="#portfolio">
-				<div>PORTFOLIO</div>
-			</a>
-			<a href="#contact">
-				<div>CONTACT</div>
-			</a>
-		</header>
+			<header>
+				<a href="#">
+					<img
+						src={eakLogo}
+						alt="The initials E.A.K, with E and K rotated and aligned to neatly overlap with the A inbetween"
+					/>
+				</a>
+				<a href="#portfolio">
+					<div>PORTFOLIO</div>
+				</a>
+				<a href="#contact">
+					<div>CONTACT</div>
+				</a>
+			</header>
+		</>
 	);
 }
 
